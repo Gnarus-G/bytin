7de68f66e6db3962ff4f92f21f8e5989bf15d04b
gnarus-g <g.avet.gnaritas@gmail.com>
2021-09-16 16:45:19 -0400

    fix  _http_common error in build script

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import("next").NextConfig} */
+module.exports = {
+    webpack: (config, { isServer }) => {
+        if(isServer) config.externals.push("_http_common");
+        return config;
+    },
+}
