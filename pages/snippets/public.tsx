f561e02d5352438a4b2c53303ffeaccf3e6a9336
gnarus-g <g.avet.gnaritas@gmail.com>
2021-09-27 22:37:12 -0400

    enhancement: rendering the public snippets on the server (SSR)

--- pages/snippets/public.tsx
+++ pages/snippets/public.tsx
@@ -1,8 +1,13 @@
 import SnippetGrid from "components/snippet/grid";
 import { usePublicSnippetsQuery } from "generated/graphql";
+import { withUrqlClient } from "next-urql";
 import React from "react";
 
-export default function PublicSnippets() {
+function PublicSnippets() {
   const [{ data }] = usePublicSnippetsQuery();
   return <SnippetGrid snippets={data?.snippets} />;
 }
+
+export default withUrqlClient((_ssr) => ({ url: process.env.API_ENDPOINT ?? "/api" }), {
+  ssr: true,
+})(PublicSnippets);
